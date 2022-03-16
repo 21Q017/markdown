59153d0baab12840f852f743906383cbedca2b23
21Q017 <905505155@qq.com>
2022-03-16 09:41:54 +0800

    2022.3.6

---
+++ 字节查看器.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char number1 = 1;
+    char number2 = '1';
+    printf("%d, %c, %d \n", number1, number1, number2);
+    // unsigned int num = 2,147,483,647//
+    printf("%d, %u \n", -123, -123);
+    float pi = 3.14;
+    printf("%f, %d, %c, %lf\n",pi, pi, pi, pi);
+
+    return 0;
+
+}
