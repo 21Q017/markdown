ef8fba8cc0c91a35684d0d435845bb7b427839e8
21Q017 <905505155@qq.com>
2022-03-21 13:59:56 +0800

    2022.3.6

---
+++ practice_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int i;	//这是一个计数器//
+    scanf("%d", &i);
+    getchar();
+    if(i >= 18)
+    {
+        printf("Ture\n");
+        while(i >= 18)
+        {
+	    printf("%d", i);
+	    i++;
+	{    
+    }   
+    else
+    {
+        printf("False\n");
+    }
+
+    return 0;
+}
